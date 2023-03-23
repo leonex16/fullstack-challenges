b511a4f35c149d255a24572c25b22bb7be68cb9d
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2023-03-23 11:49:40 -0300

    feat(interactive-rating-component): :sparkles: Load WC on the window

--- interactive-rating-component/src/main.ts
+++ interactive-rating-component/src/main.ts
@@ -1,2 +1,10 @@
-import './global.css'
+import './global.css';
+import { InteractiveRatingDialog } from './components/interactive-rating-dialog'
+import { ThankYouDialog } from './components/thank-you-dialog';
+import { Dialog } from './components/dialog';
 
+[
+  Dialog,
+  InteractiveRatingDialog,
+  ThankYouDialog,
+].forEach(WC => WC.mountOnWindow());
